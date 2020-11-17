56ecf5706c66a2ee55e56700ad8dfb1925f478d1
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2020-11-17 15:53:07 +0800

    控制器

--- AR_Midter_107051363/Assets/Scripts/GameManger.cs
+++ AR_Midter_107051363/Assets/Scripts/GameManger.cs
@@ -3,10 +3,19 @@
 public class GameManger : MonoBehaviour
 {
     [Header("人類")]
-    public Transform male;
-
+    public Transform Male;
     [Header("殭屍")]
     public Transform Zombie;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("旋轉速度"),Range(0f,5f)]
+    public float turn = 1.5f;
+    [Header("縮放"), Range(0.1f, 20f)]
+    public float size = 0.3f;
+    [Header("人類動畫元件")]
+    public Animator aniMale;
+    [Header("殭屍動畫元件")]
+    public Animator aniZombie;
 
     private void Start()
     {
@@ -16,5 +25,16 @@ public class GameManger : MonoBehaviour
     private void Update()
     {
         print("更新事件");
+        print(joystick.Vertical);
+
+        Zombie.Rotate(0, joystick.Horizontal*turn, 0);
+        Male.Rotate(0, joystick.Horizontal*turn, 0);
+
+        Zombie.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
+    }
+
+    public void Attack()
+    {
+        print("攻擊");
     }
 }
