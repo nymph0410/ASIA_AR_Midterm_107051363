4f1d07c9cea5f99fb2f4f1f9de2b28fbb49174a8
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2020-11-17 14:57:34 +0800

    搖桿

--- AR_Midter_107051363/Assets/Scripts/GameManger.cs
+++ AR_Midter_107051363/Assets/Scripts/GameManger.cs
@@ -2,36 +2,19 @@
 
 public class GameManger : MonoBehaviour
 {
-    [Header("羊")]
-    public Transform sheep;
+    [Header("人類")]
+    public Transform male;
 
-    [Header("企鵝")]
-    public Transform penguin;
+    [Header("殭屍")]
+    public Transform Zombie;
 
-    [Header("虛擬搖桿")]
-     public FixedJoystick joystick;
-
-    [Header("旋轉速度"),Range(1.0f,20f)]
-    public float turn = 1.5f;
-
-    [Header("縮放"), Range(0f, 5f)]
-    public float size = 0.3f;
-
-
-        private void Start()
+    private void Start()
     {
-        print("開始事件進行中");
+        print("開始事件");
     }
 
     private void Update()
     {
         print("更新事件");
-        print(joystick.Vertical);
-
-        sheep.Rotate(0, joystick.Horizontal*turn,0);
-        penguin.Rotate(0, joystick.Horizontal * turn, 0);
-
-        sheep.localScale += new Vector3(1, 1, 1) * joystick.Vertical;
-        
     }
 }
