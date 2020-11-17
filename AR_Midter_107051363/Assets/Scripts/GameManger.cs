949e30b3556b6d69eb9b082a4f9dceb9bcbb6e06
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2020-11-17 18:25:09 +0800

    改安卓黨

--- AR_Midter_107051363/Assets/Scripts/GameManger.cs
+++ AR_Midter_107051363/Assets/Scripts/GameManger.cs
@@ -8,7 +8,7 @@ public class GameManger : MonoBehaviour
     public Transform Zombie;
     [Header("虛擬搖桿")]
     public FixedJoystick joystick;
-    [Header("旋轉速度"),Range(0f,5f)]
+    [Header("旋轉速度"), Range(0f, 5f)]
     public float turn = 1.5f;
     [Header("縮放"), Range(0.1f, 20f)]
     public float size = 0.3f;
@@ -21,20 +21,39 @@ public class GameManger : MonoBehaviour
     {
         print("開始事件");
     }
-
+    public float test = 1;
+    public float test2 = 1;
     private void Update()
     {
         print("更新事件");
         print(joystick.Vertical);
 
-        Zombie.Rotate(0, joystick.Horizontal*turn, 0);
-        Male.Rotate(0, joystick.Horizontal*turn, 0);
+        Zombie.Rotate(0, joystick.Horizontal * turn, 0);
+        Male.Rotate(0, joystick.Horizontal * turn, 0);
 
         Zombie.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
-    }
 
+        Zombie.localScale = new Vector3(1,1,1)* Mathf.Clamp(Zombie.localScale.x, 0.5f, 3.5f);
+        test = (Mathf.Clamp(test2, 0, 10));
+
+        print(Mathf.Clamp(test2, 0, 10));
+    }
     public void Attack()
     {
         print("攻擊");
+        aniZombie.SetTrigger("攻擊觸發");
+
+        print("攻擊");
+        aniMale.SetTrigger("攻擊觸發");
+    }
+
+
+    public void PlayAnimation(string aniName) 
+    {
+        print(aniName);
+
+        aniZombie.SetTrigger(aniName);
+        aniMale.SetTrigger(aniName);
+      
     }
 }
